d5951a68b60336fc6796600fef59036ffbf4b5c8
carloseadac <carloseadac@gmail.com>
2022-07-11 10:59:29 -0300

    ai preto

--- APi/Controller/Controllers/UserController.cs
+++ APi/Controller/Controllers/UserController.cs
@@ -7,5 +7,19 @@ namespace Controller.Controllers;
 [Route("[controller]")]
 public class UserController : ControllerBase
 {
-    
+    [HttpPost]
+    [Route("register")]
+    public object registerUser([FromBody] User user){
+        var Id = user.save();
+        return new{
+            Nome = user.Nome,
+            Area = user.Area,
+            Datanasc = user.DataNasc,
+            Email = user.Email,
+            Senha = user.Senha,
+            Id = Id
+
+        };
+
+    }
 }
