c5bd494dd977c3b5c79be1ae06670137a60b9def
Alexandre Nikitin <alexandrenikitin@outlook.com>
2022-07-12 10:21:28 -0300

    merge

--- Front/src/app/ocorrencia-list/ocorrencia-list.component.ts
+++ Front/src/app/ocorrencia-list/ocorrencia-list.component.ts
@@ -1,14 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { OcorrenciasUser } from '../ocorrenciasUser';
+import axios from 'axios';
 @Component({
   selector: 'app-ocorrencia-list',
   templateUrl: './ocorrencia-list.component.html',
   styleUrls: ['./ocorrencia-list.component.css']
 })
 export class OcorrenciaListComponent implements OnInit {
-
+  
+  ocorrencias: Array<OcorrenciasUser> = [];
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.initialize();
   }
 
+  async initialize() {
+    var config = {
+      method: 'get',
+      url: 'http://localhost:5051/Ocorrencia/getAll',
+      headers: {},
+      data: '',
+    };
+
+    var instance = this;
+    axios(config).then(function (response) {
+      instance.ocorrencias = response.data;
+      console.log(instance.ocorrencias);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
 }
