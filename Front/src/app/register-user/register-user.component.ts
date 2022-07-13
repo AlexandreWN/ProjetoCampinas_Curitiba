1d8b156f83287a1a84b6a90d5cd88d490c9ddd74
carloseadac <carloseadac@gmail.com>
2022-07-13 08:42:20 -0300

    register user ok

--- Front/src/app/register-user/register-user.component.ts
+++ Front/src/app/register-user/register-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import axios from "axios";
+
 
 @Component({
   selector: 'app-register-user',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+
+  save(){
+
+    let nome = document.getElementById("nome") as HTMLInputElement;
+    let edv = document.getElementById("edv") as HTMLInputElement;
+    let email = document.getElementById("email") as HTMLInputElement;
+    let area = document.getElementById("area") as HTMLInputElement;
+    let date = document.getElementById("date") as HTMLInputElement;
+    let password = document.getElementById("password") as HTMLInputElement;
+
+    var data = JSON.stringify({
+      "nome" : nome?.value,
+      "edv" : edv?.value,
+      "email" : email?.value,
+      "area" : area?.value,
+      "dataNasc" : date?.value,
+      "senha" : password?.value
+    })
+
+    let self = this;
+    var config = {
+      method: 'post',
+      url: 'http://localhost:5051/user/register',
+      headers: { 
+        'Content-Type': 'application/json'
+       },
+      data : data
+
+    }
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      alert("Registrado com sucesso!");
+      self.router.navigate(['/registerUsers'])
+    })
+    .catch(function (error) {
+      alert("Erro!");
+      console.log(error);
+    });
+
+  }
 }
